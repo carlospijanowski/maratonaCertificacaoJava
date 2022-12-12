b3e09e4585f0e762d346a147078a66d50903ec59
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-12-11 21:42:10 -0300

    atualizacao com arquivo de referencia para anotacoes spring boot

---
+++ src/main/java/br/com/seteideias/AnotacoesSpringBoot/README/README.md
@@ -0,0 +1,3 @@
+##As anotações estao em outro projeto
+
+https://github.com/carlospijanowski/parkingControl
--- src/main/java/br/com/seteideias/Jackson/service/CarService.java
+++ src/main/java/br/com/seteideias/Jackson/service/CarService.java
@@ -4,16 +4,16 @@ import br.com.seteideias.Jackson.dto.CarDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class CarService {
 
     public String javaToString(CarDto carDto) throws JsonProcessingException {
-        ObjectMapper m = new ObjectMapper();
         return new ObjectMapper().writeValueAsString(carDto);
     }
 
     public CarDto jsonToJava(String stringJson) throws JsonProcessingException {
-        ObjectMapper m = new ObjectMapper();
         return new ObjectMapper().readValue(stringJson, CarDto.class);
     }
 
