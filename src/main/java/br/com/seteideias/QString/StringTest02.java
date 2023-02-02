c8b6df440696afbabbaa38e644b2cea0005c0461
Carlos Pijanowski Cartaxo <f633120@corp.caixa.gov.br>
2023-02-02 16:35:37 -0300

    strings imutaveis

---
+++ src/main/java/br/com/seteideias/QString/StringTest02.java
@@ -0,0 +1,14 @@
+package br.com.seteideias.QString;
+
+public class StringTest02 {
+
+    public static void main(String[] args) {
+
+        String testandoTrim = "  Carlos Pijanowski";
+        String trim = testandoTrim.trim();
+        System.out.println(testandoTrim);
+        System.out.println(trim);
+    }
+
+}
+
